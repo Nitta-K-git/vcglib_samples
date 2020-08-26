a69fb4635856632ad1e6b813dd8f936a7f67abfd
Nitta-K-git <kyohei.nitta@arithmer.co.jp>
2020-08-26 15:35:33 +0900

    update template folder

--- samples/_template/main.cpp
+++ samples/_template/main.cpp
@@ -1,13 +1,28 @@
-#include<vcg/complex/algorithms/create/platonic.h>
-#include<vcg/complex/allocate.h>
+#include <vcg/complex/algorithms/create/platonic.h>
+
+using namespace vcg;
+using namespace std;
 
 class MyFace;
 class MyVertex;
 
 struct MyUsedTypes : public UsedTypes<	Use<MyVertex>::AsVertexType, Use<MyFace>::AsFaceType> {};
-class MyVertex : public Vertex< MyUsedTypes, vertex::Coord3f, vertex::BitFlags, vertex::VFAdj> {};
-class MyFace : public Face< MyUsedTypes, face::VertexRef, face::VFAdj, face::BitFlags> {};
-class MyMesh : public tri::TriMesh<vector<MyVertex>, vector<MyFace> > {};
+class MyVertex : public Vertex< MyUsedTypes, 
+		vertex::Coord3f, 
+		vertex::Normal3f,
+		vertex::BitFlags, 
+		vertex::VFAdj
+		> {};
+class MyFace : public Face< MyUsedTypes, 
+		face::VertexRef, 
+		face::Normal3f,
+		face::VFAdj, 
+		face::BitFlags
+		> {};
+class MyMesh : public tri::TriMesh<
+		vector<MyVertex>, 
+		vector<MyFace> 
+		> {};
 
 int main(){
 	MyMesh mesh;
