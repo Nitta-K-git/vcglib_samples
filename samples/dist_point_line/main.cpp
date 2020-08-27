ebf2f66f1faba440ed97ad3881361ed575f53ae0
Nitta-K-git <kyohei.nitta@arithmer.co.jp>
2020-08-27 16:32:06 +0900

    add distance between point and line sample

---
+++ samples/dist_point_line/main.cpp
@@ -0,0 +1,19 @@
+#include <vcg/complex/complex.h>
+
+using namespace vcg;
+using namespace std;
+
+int main(){
+	Point3f A0(0,0,0), A1(10,0,0);
+	vcg::Line3f line(A0, A1-A0);
+	
+	Point3f p(20,10,0);
+	
+	Point3f B = line.ClosestPoint(p);
+	float a = vcg::Distance(line, p);
+	
+	printf("(%f,%f,%f)\n", B.X(), B.Y(), B.Z());
+	cout << a << endl;
+	
+	return 1;
+}
